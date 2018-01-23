bd71f7449b62f945ae8fa284cf29f714c4ed6615
Michel <mjbohn@gmail.com>
2018-01-23 13:43:34 +0100

    some minor cleaning for r0.2

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ jQuery( document ).ready(function() {
 
     jQuery(".plugin_marquee").css("width",JSINFO['plugin_marquee']['width']);
     jQuery(".plugin_marquee").css("height",JSINFO['plugin_marquee']['height']);
-    // jQuery(".plugin_marquee").css("color",JSINFO['plugin_marquee']['textcolor']);
+
     jQuery(".plugin_marquee").css("background-color",JSINFO['plugin_marquee']['background-color']);
     jQuery('.plugin_marquee').click(function () {
         $mq.marquee('toggle');
